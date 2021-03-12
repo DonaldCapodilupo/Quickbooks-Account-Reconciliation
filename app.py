9e0ed201e3b6504ee4ee08946c120dab6ee809cf
DonaldCapodilupo <Donald@Capodilupo.dev>
2021-03-12 10:39:48 -0500

    Program now loops and creates recons one at a time from the gui. However, some of the buttons have issues and I can only do the loop so many times before a abd request is sent.

--- app.py
+++ app.py
@@ -34,10 +34,15 @@ def main_Menu():
 
 
 
+accounts_That_Need_Recons_Created = []
+data = []
+
 @app.route('/ReconWindow', methods=["POST","GET"])
 def recon_Window():
     if request.method == 'POST':
+
         uploaded_file = request.files['excel_file']
+
         if uploaded_file.filename != '':
             filename = secure_filename(uploaded_file.filename)
             uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
@@ -47,48 +52,74 @@ def recon_Window():
             accounts_In_GL = accountsInGeneralLedger(filename)
             accounts_In_Directory = accountsInDirectory()
 
-            data =[]
-
-
+            #fills out data and accounts_That_Need_Recons_Created
             for account in accounts_In_GL.keys():
                 os.chdir(ROOT)
                 temp_name = account+".xlsx"
                 if temp_name in accounts_In_Directory:
                     data.append(Recon(account, accounts_In_GL[account], True))
-                    from Classes.SpreadsheetDesigns import SpreadsheetDesigner
-                    recon = SpreadsheetDesigner(temp_name)
-                    recon.moveAndCopyWorksheet()
-                    recon.insertNewAccountBalances(accounts_In_GL[account])
-
                 elif account not in accounts_In_Directory:
                     data.append(Recon(account, accounts_In_GL[account],False))
-                    from Classes.SpreadsheetCreator import SpreadsheetCreator
-                    recon = SpreadsheetCreator(temp_name)
-                    recon.doubleAccountReconcilliationformat(accounts_In_GL[account])
-
+                    accounts_That_Need_Recons_Created.append(Recon(account, accounts_In_GL[account],False))
 
                 else:
                     data.append(Recon(account, accounts_In_GL[account], False))
 
-            return render_template('ReconWindow.html', data=data)
-                
-
-
-
-
-
-
+        return render_template('ReconWindow.html', data=data)
 
 
     elif request.method == "GET":
         return render_template("ReconWindow.html")
 
 
+@app.route('/SingleRecon', methods=["POST","GET"])
+def create_Recon_Single():
+    if request.method == 'POST':
+        for account in accounts_That_Need_Recons_Created:
+            os.chdir(ROOT)
+
+            if request.form['Single_Recon'] == "Assign Single Reconciliation " + account.account_Name:
+                from Classes.SpreadsheetCreator import SpreadsheetCreator
+                from BackEndLogic import Recon
+                recon = SpreadsheetCreator(account.account_Name + ".xlsx")
+                recon.singleAccountReconcilliationformat(account.account_Balance)
+                new_Recon_Obj = Recon(account.account_Name, account.account_Balance, True)
+                for index, obj in enumerate(data):
+                    if obj.account_Name == account.account_Name:
+                        del data[index]
+
+                data.append(new_Recon_Obj)
+            accounts_That_Need_Recons_Created.remove(account)
+
+            return render_template('ReconWindow.html', data=data)
 
 
+    else:
+        return render_template('ReconWindow.html', data=data)
 
 
+@app.route('/DoubleRecon', methods=["POST","GET"])
+def create_Recon_Double():
+    if request.method == 'POST':
+        for account in accounts_That_Need_Recons_Created:
+            os.chdir(ROOT)
 
+            if request.form['Double_Recon'] == "Assign Double Reconciliation " + account.account_Name:
+                from Classes.SpreadsheetCreator import SpreadsheetCreator
+                from BackEndLogic import Recon
+                recon = SpreadsheetCreator(account.account_Name + ".xlsx")
+                recon.doubleAccountReconcilliationformat(account.account_Balance)
+                new_Recon_Obj = Recon(account.account_Name,account.account_Balance,True)
+                for index, obj in enumerate(data):
+                    if obj.account_Name == account.account_Name:
+                        del data[index]
+
+
+                data.append(new_Recon_Obj)
+            accounts_That_Need_Recons_Created.remove(account)
+        return render_template('ReconWindow.html', data=data)
+    else:
+        return render_template('ReconWindow.html', data=data)
 
 
 if __name__ == '__main__':
